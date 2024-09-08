53dcb4789ce7bb8b85021dfa8977a661650e05a4
Pravv13 <gpravasta13@gmail.com>
2024-09-08 21:19:53 +0700

    Menambahkan fungsi volume kubus

--- app.py
+++ app.py
@@ -4,3 +4,10 @@ def persegi():
     print("Luas Persegi adalah", luas)
 
 persegi()
+
+def kubus():
+    x = int(input("Masukkan panjang sisi" ))
+    volume = x*x*x
+    print("Volume kubus adalah: ", volume)
+
+kubus()
